8db76858439ec79f27a32dcc9cb93869a0ce2d86
Emmanuel Asaber <emmanuel.asaber@amalitech.org>
2021-01-15 15:18:27 +0000

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int addition(int a, int b, int c) {
+        return a + b + c;
+    }
+}
